f6d5eeb34bdf7ab5b1856f7093530c3483a9ee16
Frans Kurniawan <fransraharja@yahoo.com>
2017-11-24 16:49:13 +0700

    Last Revision

--- frontend/src/form/homepage/components/posts.js
+++ frontend/src/form/homepage/components/posts.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 import {withRouter} from 'react-router-dom'
 import {Button, Card, Icon, Container, Divider} from 'semantic-ui-react'
 import formatTimestamp from 'src/utils/format-timestamp'
+import {deletePost, votePost} from 'src/actions/post'
 import marked from 'src/utils/marked'
 
 class HomepagePosts extends Component {
@@ -14,6 +15,18 @@ class HomepagePosts extends Component {
 
   goToPost = post => {
     this.props.history.push(`/${post.category}/${post.id}`)
+  }
+
+  deletePost = post => {
+    this.props
+      .deletePost({postId: post.id})
+      .then(() => this.props.history.push(`/`))
+  }
+
+  votePost = ({post,vote}) => {
+    this.props
+      .votePost({postId: post.id, vote:vote})
+      .then(() => this.props.history.push(`/`))
   }
 
   render () {
@@ -44,6 +57,33 @@ class HomepagePosts extends Component {
                   <Button
                     basic
                     compact
+                    size='tiny'
+                    floated='left'
+                    color='blue'
+                    icon='thumbs outline up'
+                    onClick={() => this.votePost({post: post, vote: 'upVote'})}
+                  />
+                  <Button
+                    basic
+                    compact
+                    size='tiny'
+                    floated='left'
+                    color='red'
+                    icon='thumbs outline down'
+                    onClick={() => this.votePost({post: post, vote: 'downVote'})}
+                  />
+                  <Button
+                    secondary
+                    compact
+                    size='tiny'
+                    icon='trash outline'
+                    floated='right'
+                    content='delete'
+                    onClick={() => this.deletePost(post)}
+                  />
+                  <Button
+                    basic
+                    compact
                     color='blue'
                     size='mini'
                     floated='right'
@@ -68,6 +108,10 @@ class HomepagePosts extends Component {
 }
 
 const mapStateToProps = ({posts}) => ({posts})
-const enhance = compose(connect(mapStateToProps), withRouter)
+const mapDispatchToProps = {deletePost, votePost}
+const enhance = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withRouter
+)
 
 export default enhance(HomepagePosts)
